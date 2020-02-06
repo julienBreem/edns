6ecb2c770df76b18895353c55c742b11bd6a0cb9
jbreem <jbreem@eurodns.com>
2020-02-06 21:01:03 +0100

    display all the task

--- test.php
+++ test.php
@@ -6,10 +6,20 @@
  * Time: 20:05
  */
 
+$myTasks = array ();
+
 do {
     //print("Enter a task");
     $myTask = readline("Enter a task : ");
     //$myTask = fopen('','r');
+
     print($myTask);
+    array_push($myTasks,$myTask);
     print("\n");
+
 }while($myTask != "");
+
+foreach ($myTasks as $task) {
+    print($task);
+    print("\n");
+}
