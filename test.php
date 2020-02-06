c7886d1dfeee87ec69b9ecc3b0f017ca66aff1f4
Michael Burhin <michael.burhin@gmail.com>
2020-02-06 20:50:30 +0100

    add task + display

--- test.php
+++ test.php
@@ -1,1 +1,13 @@
 <?php
+$line = array();
+while (!isset($test) OR trim($test) !== '')
+{
+    $handle = fopen("php://stdin", "r");
+    $test = fgets($handle);
+    $line[] = $test;
+}
+fclose($handle);
+
+foreach ($line as $value) {
+    echo $value;
+}
