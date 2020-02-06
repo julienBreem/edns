6bf2030f5a3f31b2db6c387ea8f0ecf2be028355
jbreem <jbreem@eurodns.com>
2020-02-06 21:43:28 +0100

    clean code

--- test.php
+++ test.php
@@ -9,25 +9,15 @@
 $myTasks = array ();
 
 do {
-    //print("Enter a task");
     $myTask = readline("Enter a task : ");
-    //$myTask = fopen('','r');
 
-    //print($myTask);
     if($myTask != ""){
         array_push($myTasks,$myTask);
     }
 
-    //print("\n");
-
 }while($myTask != "");
 
-foreach ($myTasks as $key => $task) {
-    print($key);
-    print(" => ");
-    print($task);
-    print("\n");
-}
+showTasks($myTasks);
 
 $delete = readline("do you want to delete a task ? (y/n)");
 
@@ -35,18 +25,18 @@ while($delete === "y"){
     $id = readline("which one do you want to remove ? (number of the task)");
     unset($myTasks[$id]);
 
-    print("\n");
+    showTasks($myTasks);
 
-    foreach ($myTasks as $key => $task) {
+    $delete = readline("do you want to delete a task again ? (y/n)");
+}
+
+function showTasks($tasks){
+    foreach ($tasks as $key => $task) {
         print($key);
         print(" => ");
         print($task);
         print("\n");
-    }
-
-    $delete = readline("do you want to delete a task again ? (y/n)");
-
-}
+    }}
 
 
 
