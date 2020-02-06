245c2ac9c7ae8d1a42350d7ab5e2d6a41128175a
NameBorst <tonculte@rocketmail.com>
2020-02-06 20:33:10 +0100

    Some implementation for maging the Task entity

---
+++ Controller/CLI/taskmanager.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once($basedir. './../../Model/Entity/Task.php');
+namespace  \Controller\CLI;
+class taskmanager
+{
+    var $Taskarray[];
+
+    public static setnewtask($input){
+        //
+    }
+}
--- indexcli.php
+++ indexcli.php
@@ -1,4 +1,20 @@
 <?php
 
+    $basedir = basename(__DIR__);
+
+    exit == 1;
+    do{
+
+        echo "Type in your tasks details (or nothing to cancel this out): ";
+        $handle = fopen ("php://stdin","r");
+        $line = fgets($handle);
+        if(trim($line) != ''){
+
+        }
+        else{
+            exit == 0;
+        }
+    }while(exit);
+
 
 ?>
