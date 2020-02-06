e4910525699a11587e109bb188c14dcddd5cc954
Michael Burhin <michael.burhin@gmail.com>
2020-02-06 21:39:44 +0100

    delete items + display

--- test.php
+++ test.php
@@ -8,6 +8,33 @@ while (!isset($test) OR trim($test) !== '')
 }
 fclose($handle);
 
-foreach ($line as $value) {
+foreach ($line as $value)
+{
     echo $value;
 }
+
+while (trim($test) !=='non')
+{
+    echo'voulez vous supprimer une entrée(yes/non) ?';
+
+    $handle = fopen("php://stdin", "r");
+    $test = fgets($handle);
+    if(trim($test) == 'yes')
+    {
+        foreach ($line as $key => $value)
+        {
+            echo $key . '=>' . $value;
+        }
+        echo'choisir id a supprimer';
+
+        $handle = fopen("php://stdin", "r");
+        $test = fgets($handle);
+        unset($line[trim($test)]);
+    }
+    echo 'Voici la liste des items restant';
+    foreach ($line as $value)
+    {
+        echo $value;
+    }
+}
+
